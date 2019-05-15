01a962633315fd6727daccce78886b025706abe8
Jacob Storer <jacobstr117@gmail.com>
2019-05-15 01:54:30 -0400

    Upload from Github Desktop
    -- Added .gitignore
        -- Ignore .DS_Store / .txt files / .env / node_modules folder

--- ITbot/commands/kick.js
+++ ITbot/commands/kick.js
@@ -5,17 +5,19 @@ module.exports = message => {
   console.log("==> Entered kick.js function");
   const member = message.mentions.members.first()
 
+  // Error detection -- No member targetted
   if (!member) {
     console.log("ERROR: Didn't mention members' name.");
     console.log("~~ Early termination of function.");
     return message.reply(`Who are you trying to kick? You must mention a user.`)
-  }
+  } // End -- No member targetted
 
+  // Error detection -- Member non-kickable
   if (!member.kickable) {
     console.log("ERROR: Member isn't kickable.");
     console.log("~~ Early termination of function.");
     return message.reply(`I can't kick this user. Sorry!`)
-  }
+  } // End -- Member non-kickable
 
   console.log("~~ kick.js function received appropriate parameters... Kicking user.");
   console.log("<== Exiting kick.js function");
--- ITbot/events/message.js
+++ ITbot/events/message.js
@@ -1,10 +1,15 @@
-const kick = require('../commands/kick')
+const kick = require('../commands/kick');
+// Add new command here.. 
 
 module.exports = (client, message) => {
   console.log("==> Entered message.js function");
+  // Begin -- !kick command
   if (message.content.startsWith('!kick')) {
     console.log("Message beings with !kick");
     return kick(message)
-  }
+  } // End -- !kick command
+
+  // Add logic to detect new command here...
+
   console.log("<== Exited message.js function");
 }
